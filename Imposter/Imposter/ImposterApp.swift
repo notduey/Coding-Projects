f3272878bdb05a54e8ad67b5b3b681c93601e7a5
notduey <notduey@gmail.com>
2025-10-12 23:16:59 -0700

    added base Imposter files

---
+++ Imposter/Imposter/ImposterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImposterApp.swift
+//  Imposter
+//
+//  Created by Duy Tran on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ImposterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
